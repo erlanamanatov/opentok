00e46872ca51358656f5ba21376b17f13b0c9d90
erlanamanatov <erlanamanatov@mail.ru>
2017-06-29 22:12:22 +0600

    listener added to get TOKEN for current user

--- app/src/main/java/com/erkprog/opentok/Room.java
+++ app/src/main/java/com/erkprog/opentok/Room.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Room extends AppCompatActivity implements View.OnClickListener  {
     String userId;
     private static final String TAG = "myLogs";
+    private DatabaseReference sessionsRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
@@ -23,6 +28,39 @@ public class Room extends AppCompatActivity implements View.OnClickListener  {
         Log.d(TAG, "Room activity created");
         userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         findViewById(R.id.createRoom).setOnClickListener(this);
+
+        sessionsRef = FirebaseDatabase.getInstance().getReference("Sessions");
+        sessionsRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                if (dataSnapshot.hasChild(userId)){
+                    String token = (String) dataSnapshot.child(userId).getValue();
+                    Log.d(TAG, "============= TOKEN :" + token);
+                }
+
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
