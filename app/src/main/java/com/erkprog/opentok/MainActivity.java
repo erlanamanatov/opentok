81cdbe3bb14b0fc9b80b1432422646120a97425c
erlanamanatov <erlanamanatov@mail.ru>
2017-06-29 13:22:41 +0600

    After steps in tutorial

--- app/src/main/java/com/erkprog/opentok/MainActivity.java
+++ app/src/main/java/com/erkprog/opentok/MainActivity.java
@@ -1,20 +1,128 @@
 package com.erkprog.opentok;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.FrameLayout;
+
+import com.opentok.android.OpentokError;
+import com.opentok.android.Publisher;
+import com.opentok.android.PublisherKit;
+import com.opentok.android.Session;
+import com.opentok.android.Stream;
+import com.opentok.android.Subscriber;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  Session.SessionListener,  PublisherKit.PublisherListener {
     private static String API_KEY = "45903602";
     private static String SESSION_ID = "2_MX40NTkwMzYwMn5-MTQ5ODcyMDEwMzk0MX5GTm1aN2kxMDJwSFg0ZFhqbUhLNTVKdi9-fg";
     private static String TOKEN = "T1==cGFydG5lcl9pZD00NTkwMzYwMiZzaWc9NDNiNjI2YTQ5NjAwOWJmMTE3N2QxODM5NDAxMDIwZjdkYTI2MzQ3NzpzZXNzaW9uX2lkPTJfTVg0ME5Ua3dNell3TW41LU1UUTVPRGN5TURFd016azBNWDVHVG0xYU4ya3hNREp3U0ZnMFpGaHFiVWhMTlRWS2RpOS1mZyZjcmVhdGVfdGltZT0xNDk4NzIwMTM3Jm5vbmNlPTAuMTc5NDE2Njk0MDM5MjQxNDMmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTQ5ODc0MTczNQ==";
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private static final int RC_SETTINGS_SCREEN_PERM = 123;
     private static final int RC_VIDEO_APP_PERM = 124;
+    private Session mSession;
+    private Publisher mPublisher;
+    private Subscriber mSubscriber;
+    private FrameLayout mPublisherViewContainer;
+    private FrameLayout mSubscriberViewContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestPermissions();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @AfterPermissionGranted(RC_VIDEO_APP_PERM)
+    private void requestPermissions() {
+        String[] perms = { Manifest.permission.INTERNET, Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO };
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            // initialize view objects from your layout
+            mPublisherViewContainer = (FrameLayout)findViewById(R.id.publisher_container);
+            mSubscriberViewContainer = (FrameLayout)findViewById(R.id.subscriber_container);
+
+
+            // initialize and connect to the session
+            mSession = new Session.Builder(this, API_KEY, SESSION_ID).build();
+            mSession.setSessionListener(this);
+            mSession.connect(TOKEN);
+
+
+        } else {
+            EasyPermissions.requestPermissions(this, "This app needs access to your camera and mic to make video calls", RC_VIDEO_APP_PERM, perms);
+        }
+    }
+
+
+    @Override
+    public void onConnected(Session session) {
+        Log.d(LOG_TAG, "Session Connected");
+    }
+
+    @Override
+    public void onDisconnected(Session session) {
+        Log.d(LOG_TAG, "Session Disconnected");
+
+        mPublisher = new Publisher.Builder(this).build();
+        mPublisher.setPublisherListener(this);
+
+        mPublisherViewContainer.addView(mPublisher.getView());
+        mSession.publish(mPublisher);
+    }
+
+    @Override
+    public void onStreamReceived(Session session, Stream stream) {
+        Log.d(LOG_TAG, "Stream Received");
+
+        if (mSubscriber == null) {
+            mSubscriber = new Subscriber.Builder(this, stream).build();
+            mSession.subscribe(mSubscriber);
+            mSubscriberViewContainer.addView(mSubscriber.getView());
+        }
+    }
+
+    @Override
+    public void onStreamDropped(Session session, Stream stream) {
+        Log.d(LOG_TAG, "Stream Dropped");
+
+        if (mSubscriber != null) {
+            mSubscriber = null;
+            mSubscriberViewContainer.removeAllViews();
+        }
+    }
+
+    @Override
+    public void onError(Session session, OpentokError opentokError) {
+        Log.d(LOG_TAG, "Session error: " + opentokError.getMessage());
+    }
+
+    // PublisherListener methods
+
+    @Override
+    public void onStreamCreated(PublisherKit publisherKit, Stream stream) {
+        Log.d(LOG_TAG, "Publisher onStreamCreated");
+    }
+
+    @Override
+    public void onStreamDestroyed(PublisherKit publisherKit, Stream stream) {
+        Log.d(LOG_TAG, "Publisher onStreamDestroyed");
+    }
+
+    @Override
+    public void onError(PublisherKit publisherKit, OpentokError opentokError) {
+        Log.d(LOG_TAG, "Publisher error: " + opentokError.getMessage());
     }
 }
